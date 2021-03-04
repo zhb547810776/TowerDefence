723060d44f33b62f9fe5402d5927e5fa8c6e348b
zhb547810776 <547810776@qq.com>
2021-03-05 02:39:17 +0800

    第一日

---
+++ Assets/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Transform[] positions;
+    private int index = 0;
+    public float EnemySpeed = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = WayPoints.positions;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (index > positions.Length-1)
+        {
+            Debug.Log("到达终点");
+            return;
+        }
+        transform.Translate((positions[index].position - transform.position).normalized * Time.deltaTime * EnemySpeed);
+
+        if(Vector3.Distance(positions[index].position , transform.position) < 0.2f)
+        {
+            index++;
+            
+        }
+    }
+}
